142b737eddeda223a0a38a05f7382e21ba7328d3
Sabin Ciurusniuc <saciurus@f4r7s6.clusters.42paris.fr>
2025-06-30 18:54:05 +0200

    d

--- minitest/testmini/mini.h
+++ minitest/testmini/mini.h
@@ -7,6 +7,6 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-char	**ft_split(char const *s, char c);
+//char	**ft_split(char const *s, char c);
 
 #endif
--- minitest/testmini/quotes.c
+++ minitest/testmini/quotes.c
@@ -1,5 +1,15 @@
 #include "mini.h"
 
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
 int	count_words(char *string, char sep)
 {
 	int	i;
@@ -23,9 +33,51 @@ int	count_words(char *string, char sep)
 	return (count);
 }
 
-char	**ft_split(char *string, char sep)
+int	len_word(char *string, char sep, int i)
+{
+	int	len;
+	
+	len = 0;
+
+	while (string[i] && string[i] != sep)
+	{
+		len++;
+		i++;
+	}
+	return (len);
+}
+
+
+void	create_tab(char **split, char *string, char sep)
 {
 	int	i;
+	int	j;
+	int	k;
+	int	word_len;
+
+	i = 0;
+	k = 0;
+	while (string[i])
+	{
+		while (string[i] == sep)
+			i++;
+		if (string[i] == '\0')
+			break;
+		word_len = len_word(string, sep, i);
+		split[k] = malloc(sizeof(char) * (word_len + 1));
+		if (!split[k])
+			return;
+		j = 0;
+		while (j < word_len)
+			split[k][j++] = string[i++];
+		split[k][j] = '\0';
+		k++;
+	}
+	split[k] = NULL;
+}
+
+char	**ft_split(char *string, char sep)
+{
 	int	count;
 	char	**split;
 
@@ -33,4 +85,21 @@ char	**ft_split(char *string, char sep)
 	split = malloc(sizeof(char *) * (count + 1));
 	if (!split)
 		return (NULL);
+	create_tab(split, string, sep);
+	return(split);
+}
+#include <stdio.h>
+int	main()
+{
+	int	i;
+	char	**split;
+
+	i = 0;
+	split = ft_split("salut je vais bien", ' ');
+	while (split[i])
+	{
+		printf("%s\n", split[i]);
+		i++;
+	}
+	return (0);
 }
