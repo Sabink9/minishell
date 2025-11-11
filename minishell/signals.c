259dd450ea7035f152e1677f7dc40dc8368fd95c
Kamil Berrahou-Boisseau <kberraho@f1r5s3.clusters.42paris.fr>
2025-11-11 15:57:15 +0100

    bite

--- minishell/signals.c
+++ minishell/signals.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   signals.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sab <sab@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: kberraho <kberraho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/06 20:00:44 by sab               #+#    #+#             */
-/*   Updated: 2025/11/06 20:19:00 by sab              ###   ########.fr       */
+/*   Updated: 2025/11/11 15:53:50 by kberraho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,16 +20,10 @@ void	sigint_handler(int sig)
 {
 	(void)sig;
 	g_sig = SIGINT;
-	write(1, "\n", 1);
+	write(STDOUT_FILENO, "\n", 1);
 	rl_replace_line("", 0);
 	rl_on_new_line();
 	rl_redisplay();
-	rl_done = 1;
-}
-
-static void	sigquit_handler(int sig)
-{
-	(void)sig;
 }
 
 void	setup_interactive_signals(void)
@@ -41,8 +35,8 @@ void	setup_interactive_signals(void)
 	sa.sa_handler = sigint_handler;
 	sigaction(SIGINT, &sa, NULL);
 	sigemptyset(&sa.sa_mask);
-	sa.sa_flags = SA_RESTART;
-	sa.sa_handler = sigquit_handler;
+	sa.sa_flags = 0;
+	sa.sa_handler = SIG_IGN;
 	sigaction(SIGQUIT, &sa, NULL);
 }
 
@@ -51,3 +45,4 @@ void	setup_child_signals(void)
 	signal(SIGINT, SIG_DFL);
 	signal(SIGQUIT, SIG_DFL);
 }
+
